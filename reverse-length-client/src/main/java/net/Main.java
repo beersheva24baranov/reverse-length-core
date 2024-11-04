65386bfd2b17e9118f514c2c059de0d5ba7178b8
AlexanderBaranov <avbc@mail.ru>
2024-11-04 15:17:38 +0200

    HW # 37

--- reverse-length-client/src/main/java/net/Main.java
+++ reverse-length-client/src/main/java/net/Main.java
@@ -1,7 +1,48 @@
 package net;
+import view.*;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Main {
+    static ReverseLengthClient echoClient;
+    private static final HashSet<String> types = new HashSet<>(Arrays.asList("normal", "reverse", "length"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Item[] items = {
+                Item.of("start session", Main::startSession),
+                Item.of("exit", Main::Exit, true)
+        };
+        Menu menu = new Menu("Reverse-Length Client Application", items);
+        menu.perform(new StandardInputOutput());
+    }
+
+    static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname");
+        int port = io.readNumberRange("Enter port", "Wrong port", 3000, 50000).intValue();
+        if (echoClient != null) {
+            echoClient.close();
+        }
+        echoClient = new ReverseLengthClient(host, port);
+
+        Menu menu = new Menu("Run Session",
+                Item.of("Send request", Main::stringProcessing),
+                Item.ofExit());
+        menu.perform(io);
+    }
+
+    static void Exit(InputOutput io) {
+        if (echoClient != null) {
+            echoClient.close();
+        }
+    }
+
+    static void stringProcessing(InputOutput io) {
+        String typeOptions = String.join(", ", types);
+        String type = io.readStringOptions("Enter request type (" + typeOptions + ")",
+                "Invalid type, please choose again", types);
+        String input = io.readString("Enter string for processing");
+        String request = type + ":" + input;
+        String response = echoClient.sendAndReceive(request);
+        io.writeLine("Response: " + response);
     }
 }
--- reverse-length-server/src/main/java/net/Main.java
+++ reverse-length-server/src/main/java/net/Main.java
@@ -1,7 +1,56 @@
 package net;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final int PORT = 8081;
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started on port " + PORT);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            runSession(socket);
+        }
+    }
+
+    private static void runSession(Socket socket) {
+        Map<String, Function<String, String>> handlers = getHandlers();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintStream writer = new PrintStream(socket.getOutputStream())) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":", 2);
+                if (parts.length < 2) {
+                    writer.println("Invalid request format. Use: <type>:<string>");
+                } else {
+                    String type = parts[0].trim();
+                    String data = parts[1];
+
+                    Function<String, String> handler = handlers.get(type);
+                    if (handler != null) {
+                        writer.println(handler.apply(data));
+                    } else {
+                        writer.println("Unknown request type: " + type);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Client closed connection abnormally.");
+        }
+    }
+
+    private static Map<String, Function<String, String>> getHandlers() {
+        Map<String, Function<String, String>> handlers = new HashMap<>();
+        handlers.put("reverse", data -> new StringBuilder(data).reverse().toString());
+        handlers.put("length", data -> String.valueOf(data.length()));
+        handlers.put("normal", data -> data);
+        return handlers;
     }
 }
